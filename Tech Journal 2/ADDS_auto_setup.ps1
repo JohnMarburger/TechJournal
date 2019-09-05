ff793cf4641d0714d8772ffba9b10089366b063c
JohnMarburger <54755706+JohnMarburger@users.noreply.github.com>
2019-09-04 20:38:23 -0400

    Add files via upload
    recreated the powershell script

---
+++ Tech Journal 2/ADDS_auto_setup.ps1
@@ -0,0 +1,16 @@
+﻿#
+# Windows PowerShell script fort AD DS Deployment
+#
+Import-Module ADDSDeployment
+Install-ADDSForest `
+-CreateDnsDelegation:$false `
+-databsePath "C:\Windows\NTDS" `
+-DomainMode "WinThreshold" `
+-DomainName "john.local" `
+-DomainNetbiosName "JOHN" `
+-ForestMode "WinThreshold" `
+-InstallDns:$true `
+-LogPath "C:\Windows\NTDS" `
+-NoRebootOnCompletion:$false `
+-SysvolPath "C:\Windows\SYSVOL" `
+-Force:$true
